3e397102ea3e97eb77c0498a7077d33ed7dd5aad
natolmao <natecgilliland@gmail.com>
2024-07-09 09:27:22 +1000

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function searchMovies() {
     const movieName = document.getElementById('movieName').value;
     console.log(`Searching for: ${movieName}`);
-    // Add your search logic here
     alert(`Searching for: ${movieName}`);
 }
 
 function handleEnterKeyPress(event) {
     if (event.key === 'Enter') {
         searchMovies();
+        document.getElementById('movieName').blur();
     }
 }
 
