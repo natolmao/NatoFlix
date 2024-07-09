cb98241ea52e10009e608ac3bde7a7e8d90e3902
natolmao <natecgilliland@gmail.com>
2024-07-09 16:45:50 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 let currentPage = 1;
 const moviesPerPage = 10;
+const apiKey = '212011c';
+
+document.addEventListener('DOMContentLoaded', function() {
+    populateYearOptions();
+    document.getElementById('searchButton').addEventListener('click', () => searchMovies());
+
+    // Add an event listener to the input fields to listen for the Enter key
+    const inputFields = document.querySelectorAll('#movieName, #genre, #year, #sort');
+    inputFields.forEach(field => {
+        field.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                event.preventDefault(); // Prevent the default form submission
+                searchMovies();
+            }
+        });
+    });
+});
+
+function populateYearOptions() {
+    const yearSelect = document.getElementById('year');
+    const currentYear = new Date().getFullYear();
+    for (let year = currentYear; year >= 1900; year--) {
+        const option = document.createElement('option');
+        option.value = year;
+        option.textContent = year;
+        yearSelect.appendChild(option);
+    }
+}
 
 async function searchMovies(page = 1) {
     currentPage = page;
@@ -15,7 +43,6 @@ async function searchMovies(page = 1) {
     if (sort) query += `&sort=${sort}`;
     query += `&page=${page}`;
 
-    const apiKey = '212011c';
     const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}${query}`);
     const data = await response.json();
 
@@ -43,6 +70,11 @@ async function searchMovies(page = 1) {
     document.getElementById('genre').blur();
     document.getElementById('year').blur();
     document.getElementById('sort').blur();
+}
+
+function moveSearchContainer() {
+    const searchContainer = document.querySelector('.search-container');
+    searchContainer.style.marginBottom = '20px'; // Adjust as needed
 }
 
 function displayPagination(totalPages) {
@@ -81,14 +113,3 @@ function displayPagination(totalPages) {
         pagination.appendChild(createPageButton('Next', currentPage + 1));
     }
 }
-
-// Add an event listener to the input fields to listen for the Enter key
-const inputFields = document.querySelectorAll('#movieName, #genre, #year, #sort');
-inputFields.forEach(field => {
-    field.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-            event.preventDefault(); // Prevent the default form submission
-            searchMovies();
-        }
-    });
-});
