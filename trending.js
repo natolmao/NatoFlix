47276402440d7c294ae01c18157dda807a057194
natolmao <natecgilliland@gmail.com>
2024-09-03 08:32:34 +1000

    Update trending.js

--- trending.js
+++ trending.js
@@ -17,3 +17,33 @@ async function fetchTrendingContent() {
         console.error('Error fetching trending content:', error);
     }
 }
+
+async function displayTrendingContent() {
+    const { movies, tvShows } = await fetchTrendingContent();
+
+    const moviesContainer = document.getElementById('trending-movies');
+    const tvShowsContainer = document.getElementById('trending-tv-shows');
+
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie-item');
+        movieElement.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+            <a href="https://vidsrc.net/embed/${movie.imdb_id}" target="_blank">${movie.title}</a>
+        `;
+        moviesContainer.appendChild(movieElement);
+    });
+
+    tvShows.forEach(tvShow => {
+        const tvElement = document.createElement('div');
+        tvElement.classList.add('movie-item');
+        tvElement.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${tvShow.poster_path}" alt="${tvShow.name}">
+            <a href="https://vidsrc.net/embed/${tvShow.imdb_id}" target="_blank">${tvShow.name}</a>
+        `;
+        tvShowsContainer.appendChild(tvElement);
+    });
+}
+
+// Call the function to display trending content on page load
+displayTrendingContent();
